425d87312c9469daa019fb0982e1cc0471560de8
Ramazan Çetinkaya <75214035+ramazancetinkaya@users.noreply.github.com>
2023-01-23 16:33:28 +0300

    Create FileSizeConverter.php

---
+++ FileSizeConverter.php
@@ -0,0 +1,28 @@
+<?php
+
+class FileSizeConverter
+{
+    private const UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    private const PRECISION = 2;
+    private const MAX_SIZE = PHP_INT_MAX;
+    private const MIN_SIZE = 0;
+
+    public function convert(int $size): string
+    {
+        if ($size < self::MIN_SIZE) {
+            throw new \InvalidArgumentException("Size should be greater than or equal to 0.");
+        }
+
+        if ($size > self::MAX_SIZE) {
+            throw new \InvalidArgumentException("Size should be less than or equal to " . self::MAX_SIZE . ".");
+        }
+
+        $i = floor(log($size, 1024));
+        return round($size / (1024 ** $i), self::PRECISION) . ' ' . self::UNITS[$i];
+    }
+}
+
+// You can use this class in your code like this:
+$converter = new FileSizeConverter();
+$size = $converter->convert(12345678);
+// Output: "11.77 MB"
